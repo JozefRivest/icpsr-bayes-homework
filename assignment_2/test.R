ed5eef52393ad44436d18549574a832dfb39ecda
Jozef Rivest <jrivest@umich.edu>
2026-06-23 13:52:54 -0400

    Drafts of code for the second homework

---
+++ assignment_2/test.R
@@ -0,0 +1,60 @@
+library(rstan)
+library(bayesplot)
+library(broom)
+library(ggplot2)
+
+load("assignment_2/birthdata.Rda")
+
+dat <- birth.data
+head(dat)
+
+model.list <- list(
+  N = nrow(dat),
+  birth_1 = dat$birth1,
+  birth_2 = dat$birth2
+)
+
+fit <- stan(file = "assignment_2/model.stan", data = model.list)
+
+print(fit)
+
+trace_plot <- mcmc_trace(fit, pars = "p") +
+  ggplot2::theme(
+    text = element_text(size = 14)
+  )
+ggsave(
+  "assignment_2/figures/trace_plot_1.png",
+  plot = trace_plot,
+  width = 7,
+  height = 6
+)
+
+#############
+## Problem 2
+#############
+
+model.list.2 <- list(
+  N = nrow(dat),
+  birth_1 = dat$birth1,
+  birth_2 = dat$birth2
+)
+
+fit.2 <- stan(file = "assignment_2/model_2.stan", data = model.list.2)
+
+print(fit.2)
+
+trace_plot.2 <- mcmc_trace(fit.2, pars = c("p1", "p2")) +
+  ggplot2::theme(
+    text = element_text(size = 14)
+  )
+trace_plot.2
+ggsave(
+  "assignment_2/figures/trace_plot_2.png",
+  plot = trace_plot.2,
+  width = 9,
+  height = 4
+)
+
+#############
+## Problem 3
+#############
